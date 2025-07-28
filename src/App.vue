6d849535d7f4c9d0d39db457e3c860eed2d68f06
Daniel van der Wallen <daniel@e-ol.nl>
2025-07-28 12:17:34 +0200

    up

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div class="h-screen flex flex-col">
     <div class="flex flex-1 overflow-hidden">
-      <!-- Sidebar -->
       <FolderList @select-folder="folderId = $event" />
-
-      <!-- Main -->
       <div class="flex-1 flex flex-col">
-        <div class="p-4 border-b bg-white dark:border-gray-700 dark:bg-gray-800 dark:text-white">
-          <!-- ComposeMessage wordt nu als overlay getoond -->
-        </div>
-        <div class="flex-1 flex">
+        <div class="p-4 border-b bg-white dark:border-gray-700 dark:bg-gray-800 dark:text-white"></div>
+        <div class="flex-1 flex flex-col relative">
           <MessageList
-            class="flex-1 overflow-auto p-4"
             :folderId="folderId"
             @select-message="message = $event"
             @new-message="showCompose = true"
+            v-show="!message"
+            class="h-full"
           />
-          <MessagePane
-            class="w-1/2 border-l dark:border-gray-700"
-            v-if="message"
-            :message="message"
-          />
+          <template v-if="message">
+            <div :style="{height: listHeight + 'px'}" class="relative">
+              <MessageList
+                :folderId="folderId"
+                @select-message="message = $event"
+                @new-message="showCompose = true"
+                class="h-full"
+              />
+              <!-- Splitter -->
+              <div
+                class="absolute bottom-0 left-0 w-full h-2 cursor-row-resize bg-gray-200 dark:bg-gray-700 z-10"
+                @mousedown="startDrag"
+              ></div>
+            </div>
+            <MessagePane
+              :message="message"
+              :style="{height: 'calc(100% - ' + listHeight + 'px)'}"
+              class="border-t dark:border-gray-700"
+            />
+          </template>
         </div>
-        <!-- Overlay ComposeMessage -->
         <ComposeMessage
           v-if="showCompose"
           @close="showCompose = false"
@@ -47,7 +57,35 @@ export default {
     return {
       folderId: null,
       message: null,
-      showCompose: false
+      showCompose: false,
+      listHeight: 300, // beginhoogte in px
+      dragging: false,
+      minHeight: 100,
+      maxHeight: 600
+    }
+  },
+  methods: {
+    startDrag(e) {
+      this.dragging = true
+      document.body.style.cursor = 'row-resize'
+      window.addEventListener('mousemove', this.onDrag)
+      window.addEventListener('mouseup', this.stopDrag)
+    },
+    onDrag(e) {
+      if (!this.dragging) return
+      const container = this.$el.querySelector('.flex-1.flex.flex-col.relative')
+      const rect = container.getBoundingClientRect()
+      let newHeight = e.clientY - rect.top
+      if (newHeight < this.minHeight) newHeight = this.minHeight
+      const minPaneHeight = 100 // minimale hoogte van MessagePane
+      if (newHeight > rect.height - minPaneHeight) newHeight = rect.height - minPaneHeight
+      this.listHeight = newHeight
+    },
+    stopDrag() {
+      this.dragging = false
+      document.body.style.cursor = ''
+      window.removeEventListener('mousemove', this.onDrag)
+      window.removeEventListener('mouseup', this.stopDrag)
     }
   }
 }
